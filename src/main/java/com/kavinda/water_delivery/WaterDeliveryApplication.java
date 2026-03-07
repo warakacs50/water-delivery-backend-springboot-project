12564a7d03cc6896461a0b9a9a8b18746ee901df
kavinda warakamura <warakaofficial@gmail.com>
2026-03-08 02:27:56 +0530

    project initialization for water delivery system

---
+++ src/main/java/com/kavinda/water_delivery/WaterDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.kavinda.water_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kavinda/water_delivery/WaterDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavinda.water_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
